088dc43345ba3c7607c77aa908a36a47469a9421
oamin12 <40728676+oamin12@users.noreply.github.com>
2021-12-26 00:27:53 +0200

    640x350 resolution
    Changed Registers' sizes
    Added DS blocks

--- game.asm
+++ game.asm
@@ -1,11 +1,15 @@
 include DrawRec.inc
+include DrawDS.inc
+include P1regs.inc
+include P2regs.inc
 .model small
 .data
 xr dw ?
 yr dw ?
+
 x db ?
 y db ?
-texxt db "MOSMOS$"
+
 Lax db "AX$"
 Lbx db "BX$"
 Lcx db "CX$"
@@ -14,7 +18,8 @@ Lsi db "SI$"
 Ldi db "DI$"
 Lsp db "SP$"
 Lbp db "BP$"
-; trycatch db "0000$"
+db "$$$"
+trycatch db "0000$"
 
 
 
@@ -26,229 +31,49 @@ main proc far
     mov ds,ax
      
     mov ah,0
-    mov al,13h  ;; 640x320
+    mov al,10h  ;;10h 640x350
     int 10h
 
-    mov al,0fh
+    mov al,03h
     mov ah,0ch
 
-    mov cx,160
+    mov cx,320
     mov dx,0
     loop1: ;split screen into 2 players' screen
     int 10h
     inc dx
-    cmp dx,115
+    cmp dx,200
     jnz loop1
 
     mov cx,0  ;split for bottons bel 3ard
-    mov dx,115
+    mov dx,200
     loop2:
     int 10h
     inc cx
-    cmp cx,320
+    cmp cx,640
     jnz loop2
 
-    ;kol el arqam el hatetbe3 gowa el box ba3d makan esmo be +3    
+    mov cx,0  ;Draw Chat Bar
+    mov dx,300
+    loop33:
+    int 10h
+    inc cx
+    cmp cx,640
+    jnz loop33
 
-    ; mov ah,2
-    ; mov dl,24
-    ; mov dh,2
-    ; int 10h      
-    ; mov ah,9
-    ; mov dx,offset trycatch
-    ; int 21h
 
-    ;Drawing rectangles
-    call drawRegsP1
-    call drawRegsP2
+    ;Drawing Data Segment
+    
+
+    ;Drawing Registers
+    P1regs
+    P2regs
+    DrawDS
+   
    
 
 main endp
 
-drawRegsP1 proc NEAR
-    ;Drawing Player 1 Boxes
-    mov xr,30
-    mov yr,10
-    DrawRec xr,yr
-    
-    add yr,21
-    DrawRec xr,yr
-
-    add yr,21
-    DrawRec xr,yr
-    
-    add yr,21
-    DrawRec xr,yr
-
-    mov xr,70
-    mov yr,10
-    DrawRec xr,yr
-    
-    add yr,21
-    DrawRec xr,yr
-    
-    add yr,21
-    DrawRec xr,yr
-    
-    add yr,21
-    DrawRec xr,yr
-
-    mov ah,2
-    mov dl,1
-    mov dh,2
-    int 10h      
-    mov ah,9
-    mov dx,offset Lax
-    int 21h
-
-    mov ah,2
-    mov dl,1
-    mov dh,5
-    int 10h  
-    mov ah,9
-    mov dx,offset Lbx
-    int 21h
-
-    mov ah,2
-    mov dl,1
-    mov dh,8
-    int 10h  
-    mov ah,9
-    mov dx,offset Lcx
-    int 21h
-
-
-    mov ah,2
-    mov dl,1
-    mov dh,11
-    int 10h  
-    mov ah,9
-    mov dx,offset Ldx
-    int 21h
-
-    mov ah,2
-    mov dl,15
-    mov dh,2
-    int 10h  
-    mov ah,9
-    mov dx,offset Lsi
-    int 21h
-
-    mov ah,2
-    mov dl,15
-    mov dh,5
-    int 10h  
-    mov ah,9
-    mov dx,offset Ldi
-    int 21h
-    
-    mov ah,2
-    mov dl,15
-    mov dh,8
-    int 10h  
-    mov ah,9
-    mov dx,offset Lsp
-    int 21h
-
-    mov ah,2
-    mov dl,15
-    mov dh,11
-    int 10h  
-    mov ah,9
-    mov dx,offset Lbp
-    int 21h
-drawRegsP1 endp
-
-drawRegsP2 proc NEAR
-
-    mov xr,190
-    mov yr,10
-    
-    DrawRec xr,yr
-    
-    add yr,21
-    DrawRec xr,yr
-    
-    add yr,21
-    DrawRec xr,yr
-    add yr,21
-    DrawRec xr,yr
-
-    mov xr,230
-    mov yr,10
-    DrawRec xr,yr
-    add yr,21
-    DrawRec xr,yr
-    add yr,21
-    DrawRec xr,yr
-    add yr,21
-    DrawRec xr,yr
-
-    mov ah,2
-    mov dl,21
-    mov dh,2
-    int 10h      
-    mov ah,9
-    mov dx,offset Lax
-    int 21h
-
-    mov ah,2
-    mov dl,21
-    mov dh,5
-    int 10h  
-    mov ah,9
-    mov dx,offset Lbx
-    int 21h
-
-    mov ah,2
-    mov dl,21
-    mov dh,8
-    int 10h  
-    mov ah,9
-    mov dx,offset Lcx
-    int 21h
-
-
-    mov ah,2
-    mov dl,21
-    mov dh,11
-    int 10h  
-    mov ah,9
-    mov dx,offset Ldx
-    int 21h
-
-    mov ah,2
-    mov dl,35
-    mov dh,2
-    int 10h  
-    mov ah,9
-    mov dx,offset Lsi
-    int 21h
-
-    mov ah,2
-    mov dl,35
-    mov dh,5
-    int 10h  
-    mov ah,9
-    mov dx,offset Ldi
-    int 21h
-    
-    mov ah,2
-    mov dl,35
-    mov dh,8
-    int 10h  
-    mov ah,9
-    mov dx,offset Lsp
-    int 21h
-
-    mov ah,2
-    mov dl,35
-    mov dh,11
-    int 10h  
-    mov ah,9
-    mov dx,offset Lbp
-    int 21h
-drawRegsP2 endp
 
 
 end main
